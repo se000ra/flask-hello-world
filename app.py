ea5283b267d01915b99a012ec936751c63b522c0
se000ra <se000ra.gmail.com>
2014-10-23 15:52:59 +0600

    test variable rules

--- app.py
+++ app.py
@@ -19,6 +19,40 @@ app = Flask(__name__)
 def hello_world():
     return "Hello, World!"
 
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+
+#@app.route("/integer/<int:v>")
+#@app.route("/integer/<int:value>")
+#def int_type(value):
+    #print value+1
+    ##print v*v
+    #return "ok"
+@app.route("/integer/<int:value>")
+def int_type(value):
+    r = value ** 3
+    return 'v %d' % r
+
+@app.route("/float/<float:value>")
+def float_type(value):
+    r = value ** 3
+    return 'v %f' % r
+
+@app.route("/path/<path:value>")
+def path_type(value):
+    return 'v %s' % value 
+
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == 'mike':
+        return "Hello, {}".format(name), 200
+    else :
+        return "нет ничего", 404
+    
+
+    return search_query
 if __name__ == "__main__":
+    app.debug = True
     app.run()
 
